e95151eec9b5ad5d582558bb45230610b1ae9942
Jason Corbett <jsncbt@gmail.com>
2021-01-11 19:58:17 +1000

    Conditional scroll spy

--- static/js/scrollspy.js
+++ static/js/scrollspy.js
@@ -1,9 +1,12 @@
 window.addEventListener('DOMContentLoaded', function(event) {
   const header = document.querySelector('.header')
+  const toc = document.getElementById("TableOfContents")
 
-  var spy = new Gumshoe('#TableOfContents a', {
-    offset: () => {
-      return header.getBoundingClientRect().height + 32
-    }
-  });
+  if (toc) {
+    var spy = new Gumshoe('#TableOfContents a', {
+      offset: () => {
+        return header.getBoundingClientRect().height + 32
+      }
+    });
+  }
 }, false);
